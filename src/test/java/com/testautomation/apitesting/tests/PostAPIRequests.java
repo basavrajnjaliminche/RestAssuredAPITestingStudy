a225b725b8f9d8987c0122d249704344e809985a
basavrajnjaliminche <basavrajnjaliminche@gmail.com>
2024-02-25 18:55:27 +0100

    First commit

---
+++ src/test/java/com/testautomation/apitesting/tests/PostAPIRequests.java
@@ -0,0 +1,14 @@
+package com.testautomation.apitesting.tests;
+
+import org.testng.annotations.Test;
+
+public class PostAPIRequests {
+
+	@Test
+	public void createBooking() {
+		
+		//prepare request body
+		
+		
+	}
+}
