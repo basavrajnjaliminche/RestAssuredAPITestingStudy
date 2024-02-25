6ef656c6d02a2e8d22561bac30f7e41f3777c561
basavrajnjaliminche <basavrajnjaliminche@gmail.com>
2024-02-25 19:07:04 +0100

    data is added to PostAPIRequests

--- src/test/java/com/testautomation/apitesting/tests/PostAPIRequests.java
+++ src/test/java/com/testautomation/apitesting/tests/PostAPIRequests.java
@@ -2,6 +2,8 @@ package com.testautomation.apitesting.tests;
 
 import org.testng.annotations.Test;
 
+import net.minidev.json.JSONObject;
+
 public class PostAPIRequests {
 
 	@Test
@@ -9,6 +11,19 @@ public class PostAPIRequests {
 		
 		//prepare request body
 		//also create with pojo method 
+		JSONObject booking = new JSONObject();
+		JSONObject bookingDates = new JSONObject();
+		
+		booking.put("firstname", "api testing");
+		booking.put("lastname", "tutorial");
+		booking.put("totalprice", 1000);
+		booking.put("depositpaid", true);
+		booking.put("additionalneeds", "breakfast");
+		booking.put("bookingdates", bookingDates);
+		
+		bookingDates.put("checkin", "2023-03-25");
+		bookingDates.put("checkout", "2023-03-30");
+					
 		
 		
 	}
